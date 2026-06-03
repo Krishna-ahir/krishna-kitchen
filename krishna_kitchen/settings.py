66db603a27640f6a2779b11cc8fec63bdb2feef5
krishna <krishiahir02@gmail.com>
2026-06-03 19:47:21 +0530

    Set DEBUG false for deployment

--- krishna_kitchen/settings.py
+++ krishna_kitchen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fhs_%ki^#&k=fxes!wp1zdz0+1_(efp#9vp0t$un=dlxaq)dg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
